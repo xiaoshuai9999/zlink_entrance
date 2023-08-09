dfb85a55e09b4ac44f0d7010b4e2251b8da7233a
李帅 <lishuai1116@digiwin.com>
2023-08-09 13:50:51 +0800

    🎉 init

--- res/js/main.js
+++ res/js/main.js
@@ -1,29 +1,34 @@
-const types = [
-    {type: '通用行业', url:'https://www.baidu.com/'},
-    {type: '装备制造行业', url:''},
-    {type: 'IC行业', url:'https://www.baidu.com/'},
-    {type: 'IOT行业', url:''}
+// 通用行业
+const typeNew = [
+    {type: 'ALL', url:'http://172.31.75.154:18180'}, //全模块
+    {type: 'E10', url:'http://172.31.75.154:18180'}, // E10 Lite
+    {type: 'T100', url:'https://chaoyue8.vip'} // T100 Lite
 ]
-const body = `<div class="title"></div><div class="decoration_box"></div><div class="entrance_box">entrance_box_html</div>`;
-let content = '';
-for (let i = 0; i < types.length; i++) {
-    const item = types[i];
-    content += `<div class='entrance_item'>
-                    <div class="item_title">${item.type}</div>
-                    <div class="item_icon item_icon_${i+1}"></div>
-                    <a class="item_op">立即访问</a>
-                </div>`;
+// 其它
+const types = [
+    {type: 'EM', url:'http://172.31.75.154:18180'}, // 装备制造行业
+    {type: 'IC', url:'http://172.31.75.154:18180'}, // IC行业
+    {type: 'IOT', url:''} // IOT行业
+]
+
+const handler = function(type, isCommon = false) {
+    const t = isCommon ? typeNew : types;
+    const url = t.find(i => i.type === type)?.url
+    if (url) {
+        window.open(url);
+    } else {
+        layui.layer.msg('入口暂未开放，敬请期待');
+    }
 }
+const accessUrl = function(type) {
+    handler(type);
+}
+const accessCommonUrl = function(type) {
+    handler(type, true);
+}
+
 const banner = `关于智互联 | 联系我们 | 著作权与商标声明 | 法律声明 | 服务条款 | 隐私政策 | 网站导航  &nbsp;© Copyright 2019 智互联(深圳)科技有限公司版权所有`
+
 $(() => {
-    $('body').html(body.replace('entrance_box_html', content))
-        .append(`<div class="footer"><img class="logo" alt="logo"><span>${banner}</span></div>`);
-    $('.entrance_item').click(function() {
-        const index = $(this).index();
-        if (types[index].url) {
-            window.open(types[index].url);
-        } else  {
-            layui.layer.msg('入口暂未开放，敬请期待');
-        }
-    })
+    $('body').append(`<div class="footer"><img class="logo" alt="logo"><span>${banner}</span></div>`);
 });
